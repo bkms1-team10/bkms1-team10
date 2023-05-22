d41e3455bddde0fa09801bcd30d1afb10525f91c
Yegyu-Han <yegyuhan@gmail.com>
2023-05-22 23:16:24 +0900

    session (temporary), logout

--- server.py
+++ server.py
@@ -1,49 +1,69 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session, request
+import os
+
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/index/')
 def landing():
     ## 세션 정보 있으면 home로 이동, 없으면 landing page 출력
-    return render_template("index.html")
+    if 'userID' in session:
+        return redirect(url_for('home'))
+    else:
+        return render_template("index.html")
 
-@app.route('/login/')
-def login():
+@app.route('/login/', methods=['GET', 'POST'])
+def login():     
     ## 세션 정보 있으면 home로 이동
-    ##status로 세션 존재여부 넘기기
-    header='login'
-    return render_template("/login/login.html", status=header)
+    if 'userID' in session:
+        return redirect(url_for('home'))
+    else:
+        if request.method == 'POST':
+            ##로그인 정보 일치하는 지 확인
+            session['userID'] = request.form['userID']
+            return redirect(url_for('home'))
+        else:
+            header='login'
+            return render_template("/login/login.html", status=header)
 
 @app.route('/logout/')
 def logout():
-    print('로그아웃 되었습니다.')
+    session.pop('userID', None)
     return redirect(url_for('landing'))
 
 @app.route('/join/')
 def join():
     ## 세션 정보 있으면 home로 이동
-    header='login'
-    return render_template("/login/join.html", status=header)
+    if 'userID' in session:
+        return redirect(url_for('home'))
+    else:
+        header='login'
+        return render_template("/login/join.html", status=header)    
 
 @app.route('/')
 def home():
-    ## 세션 정보 없으면 landing page 출력
-    ##status로 세션 존재여부 넘기기
-    header='logout'
-    return render_template("/home/home.html", status=header)
+    if 'userID' in session:
+        header='logout'
+        return render_template("/home/home.html", status=header)
+    else:
+        return redirect(url_for('landing'))
 
 
 @app.route('/search/')
 def search():
-    ## 세션 정보 없으면 landing page 출력
-    ##status로 세션 존재여부 넘기기
-    header='logout'
-    return render_template("/search/search.html", status=header)
+    if 'userID' in session:
+        header='logout'
+        return render_template("/search/search.html", status=header)
+    else:
+        return redirect(url_for('landing'))
+    
 
 @app.route('/share/')
 def share():
-    ## 세션 정보 없으면 landing page 출력
-    ##status로 세션 존재여부 넘기기
-    header='logout'
-    return render_template("/share/share.html", status=header)
+    if 'userID' in session:
+        header='logout'
+        return render_template("/share/share.html", status=header)
+    else:
+        return redirect(url_for('landing'))    
 
 app.run(debug=True)

0510adbe652150c3bcda5c0342d64a5f3fd005d5
Yegyu-Han <yegyuhan@gmail.com>
2023-06-05 00:05:06 +0900

    no message

--- server.py
+++ server.py
@@ -1,20 +1,15 @@
-from flask import Flask, render_template, redirect, url_for, session, request 
+from flask import Flask, render_template, redirect, url_for, session, request, current_app , jsonify
 import os
 from flask_sqlalchemy import SQLAlchemy
 import base
+from models.user import Books ,Users
+from schemas.user import BooksSchema
 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Daniel Hanjoo Rhee/Documents/Downloads/BKMS0604/project.db/project.db'
 base.db = SQLAlchemy(app)
-
-
-from models.user import Books ,Users
-from schemas.user import BooksSchema
-from flask import session, render_template ,request , redirect , url_for , current_app , jsonify
-
-
 
 @app.route('/get_books/', methods=['GET'])
 def get_books():
@@ -62,88 +57,84 @@ def search_book():
         
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():     
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
     ## 세션 정보 있으면 home로 이동
-
-
-    if request.method == 'POST':
-        data = request.form
-
-        id = data['id']
-        pw = data['pw']
-        base.db.session.query(Users).order_by(Users.id.desc())
-        user = Users.query.filter(Users.user_id == id).first()
-
-        if user is None:
-            current_app.logger.info('%s, / : none id' % (id))
-            return 'ID_NOT_FOUND'
-        
-        if user.pw != pw :
-            current_app.logger.info('%s, / : PASSWORD_NOT_FOUND ' % (id))
-            return 'PASSWORD_NOT_FOUND'
-
-
-        session["userID"] = id
-
-
-
-        ##로그인 정보 일치하는 지 확인
-        ##1) 아이디 검색
-        ##2) 아이디+비밀번호 검색
-        ##1) 2) 결과 없으면 html 출력 시 parameter로 message 전달 ID_NOT_FOUND / PASSWORD_NOT_FOUND, header = 'login'
-        
-        ###############################
-        #message='ID_NOT_FOUND'       
-        #message='PASSWORD_NOT_FOUND'
-        #return render_template("/login/login.html", status='login', message=message)
-        ################################        
-        
-        ##2) 결과 있으면 세션 설정(하단 코드)
-
-        current_app.logger.info("로그인 성공")
-        return "success"
+    if 'userID' in session:
+        return redirect(url_for('home'))
     else:
-        header='login'
-        return render_template("/login/login.html", status=header)
+        if request.method == 'POST':
+            data = request.form
+
+            id = data['id']
+            pw = data['pw']
+            base.db.session.query(Users).order_by(Users.id.desc())
+            user = Users.query.filter(Users.user_id == id).first()
+
+            ##로그인 정보 일치하는 지 확인
+            ##1) 아이디 검색
+            if user is None:
+                current_app.logger.info('%s, / : none id' % (id))
+                return 'ID_NOT_FOUND'
+            
+            ##2) 아이디+비밀번호 검색            
+            if user.pw != pw :
+                current_app.logger.info('%s, / : PASSWORD_NOT_FOUND ' % (id))
+                return 'PASSWORD_NOT_FOUND'
+          
+            ##로그인 성공 시 세션 설정
+            session["userID"] = id
+            current_app.logger.info("로그인 성공")
+            return "success"
+        
+        else:
+            header='login'
+            return render_template("/login/login.html", status=header)
 
 
-@app.route('/register', methods=["POST"])
-def register():
-    """
-    회원가입
-    상세 주소 구 동 제외 했습니다.
-    """
 
+@app.route('/join/', methods=['GET', 'POST']))
+def join():
+    ## 세션 정보 있으면 home로 이동
+    if 'userID' in  session:
+        return redirect(url_for('home'))
+    else:
+        if request.method == 'POST':
+            """
+            회원가입
+            상세 주소 구 동 제외 했습니다.
+            """
+            
+            data = request.form
 
-    data = request.form
+            test_query = base.db.session.query(Users).order_by(Users.id.desc())
+            test_query =test_query.first()
 
-    test_query = base.db.session.query(Users).order_by(Users.id.desc())
-    test_query =test_query.first()
+            users = {}
+            users['id'] = int(test_query.id) + 1
+            users['user_id'] = data['userid']
+            users['pw'] = data['password']
+            users['nickname'] = data['username']
+            users['email'] = data['email']
+            users['lat'] = data['lat']
+            users['long'] = data['long']
+            #users['postcode'] = data['postcode']
+            #users['address'] = data['address']
+            #users['address_dong'] = ""
+            #users['lat_long'] = ""
+            #users['address_gu'] = ""
+        
+            # user = Users(nickname = data['username'], email = data['email'], pw= data['password'], address = data['address'] )
 
+            user = Users(** users)
+            base.db.session.add(user)
+            base.db.session.commit()
 
-    users = {}
-    users['id'] = int(test_query.id) + 1
-    users['user_id'] = data['userid']
-    users['pw'] = data['password']
-    users['nickname'] = data['username']
-    users['email'] = data['email']
-    # users['postcode'] = data['postcode']
-    users['address'] = data['address']
-    users['address_dong'] = ""
-    users['lat_long'] = ""
-    users['lat'] = ""
-    users['address_gu'] = ""
-    users['long'] = ""
-    # user = Users(nickname = data['username'], email = data['email'], pw= data['password'], address = data['address'] )
-
-                            
-    user = Users(** users)
-    base.db.session.add(user)
-    base.db.session.commit()
-
-
-    return 'true'
+            return redirect(url_for('login'))
+        else:
+            header='login'
+            return render_template("/login/join.html", status=header)
+        
 
 
 @app.route('/idCheck/', methods=['POST'])
@@ -177,43 +168,21 @@ def map():
     return render_template("/bookInfo/map.html")
 
 
-
-from flask import session 
-from models.user import Books 
-from schemas.user import BooksSchema
-
 @app.route('/logout/')
 def logout():
     session.pop('userID', None)
     return redirect(url_for('landing'))
 
-@app.route('/join/')
-def join():
-    ## 세션 정보 있으면 home로 이동
-    if 'userID' in  session:
-        return redirect(url_for('home'))
-    else:
-        header='login'
-        return render_template("/login/join.html", status=header)    
+    
 
 @app.route('/')
 def home():
-
     if 'userID'  in  session:
         header='logout'
         return render_template("/home/home.html", status=header)
     else:
         return redirect(url_for('landing'))
     
-
-@app.route('/login_view')
-def login_view():
-    if 'userID'  in  session:
-
-        return redirect(url_for('landing'))
-    return render_template("/login/login.html")
-
-
 
 @app.route('/search/')
 def search():
